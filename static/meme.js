15132ca5845a40395b850a7fd1f8ca3ef9cdb5fd
Tricktionary <andyaoux@hotmail.com>
2019-02-09 19:34:00 -0500

    Animation

--- static/meme.js
+++ static/meme.js
@@ -39,3 +39,26 @@ module.exports.rgbToHex = rgbToHex;
 module.exports.byteToHex = byteToHex;
 module.exports.numToHexChar = numToHexChar;
 module.exports.makeColorList = makeColorList;
+
+
+function AnimateRotate(angle) {
+	// caching the object for performance reasons
+	var $elem = $('#art');
+
+	// we use a pseudo object for the animation
+	// (starts from `0` to `angle`), you can name it as you want
+	$({deg: 0}).animate({deg: angle}, {
+		duration: 300,
+		step: function(now) {
+			// in the step-callback (that is fired each step of the animation),
+			// you can use the `now` paramter which contains the current
+			// animation-position (`0` up to `angle`)
+			$elem.css({
+				transform: 'rotate(' + now + 'deg)'
+			});
+		},
+		complete: function(){
+			AnimateRotate(360);
+		}
+	},'linear');
+}
