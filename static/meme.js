90c7c8e8bfbffb718a284b30159e3534b8afa145
Tricktionary <andyaoux@hotmail.com>
2019-02-09 19:38:05 -0500

    animation slowed down

--- static/meme.js
+++ static/meme.js
@@ -1,7 +1,7 @@
 function randomColor(){
 	var r = Math.floor(Math.random()*256);	
 	var g = Math.floor(Math.random()*256);
-	var b = Math.floor(Math.random()*256);
+    var b = Math.floor(Math.random()*256);
 	return rgbToHex(r,g,b);
 }
 
